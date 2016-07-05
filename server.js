ae1a4aa67becdba1b0c3d3e86f563dc9ebfb259e
atomicman <atomictheman@gmail.com>
2016-07-04 19:50:59 -0500

    added a small line of text followed by live stream iframe of nasa juno mission.

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
 'use strict'
 
-var http = require('http');
+
+var express = require('express');
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// // set the view engine to ejs
+// app.set('view engine', 'ejs');
+
+// // make express look in the public directory for assets (css/js/img)
+// app.use(express.static(__dirname + '/public'));
+
+// // set the home page route
+// app.get('/', function(req, res) {
+
+//     // ejs render automatically looks in the views folder
+//     res.render('index');
+// });
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
+
+
+app.get('//', function(req,res){
+	res.send('<p>Welcome to the digital dark side</p>' + 
+		'<iframe width="560" height="315" src="https://www.youtube.com/embed/OX9I1KyNa8M" frameborder="0" allowfullscreen></iframe>');
+});
+
+
 
 
